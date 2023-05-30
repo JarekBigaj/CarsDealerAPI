8c1499f23051a5423ce0c433c97beda694c9fba8
Jarek Bigaj <j.bigaj@interia.pl>
2023-05-30 08:55:36 +0200

    Initial web

---
+++ CarsDealerApi/Model/Car.cs
@@ -0,0 +1,9 @@
+public class Car 
+{
+    public int Id { get; set;}
+    public required string Make { get; set; }
+    public required string Model { get; set; }
+    public int Year { get; set; }
+    public required decimal Price { get; set; }
+    public int Mileage { get; set; }
+}
